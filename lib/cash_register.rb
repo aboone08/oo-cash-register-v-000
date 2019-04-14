cfd94ee5150a03b21f85c146f996982e1395440f
Andranique Boone <andranique.boone@gmail.com>
2019-04-14 19:07:40 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,12 @@
+class CashRegister
 
+  attr_accessor :total,  :cash_register_with_discount
+
+  def initialize(total=0)
+    @total = total
+    @cash_register_with_discount = 20.total
+  end
+
+
+
+end
