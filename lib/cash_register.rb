1960e8da034ac2c007dcd78d3df02a7f73ab41ac
Andranique Boone <andranique.boone@gmail.com>
2019-04-14 19:14:48 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,12 +1,25 @@
 class CashRegister
 
-  attr_accessor :total, :discount
+  attr_accessor :total, :discount, :items
 
   def initialize(discount=0)
     @total = 0
     @discount = discount
+    @items = []
   end
 
-
+  def add_item(title, price, quantity = 1)
+    @price = price
+    @total += price * quantity
+    if quantity > 1
+      counter = 0
+      while counter < quantity
+        @items << title
+        counter += 1
+      end
+    else
+      @items << title
+    end
+  end
 
 end
